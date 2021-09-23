0fd2d3a04a61ff0e28211e31d9f9762aec9d68cb
Garfield Lee <openpgp@550.moe>
2021-09-23 10:07:58 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# @art-economics/default-token-list
+
+This NPM module and GitHub repo contains the default token list used in the Art Economics interface.
+
+### Disclaimer
+
+Note filing an issue does not guarantee addition to this default token list.
+We do not review token addition requests in any particular order, and we do not
+guarantee that we will review your request to add the token to the default list.
+
---
+++ src/buildList.js
@@ -0,0 +1,33 @@
+const { version } = require('../package.json');
+const bsc_mainnet = require('./tokens/bsc-mainnet.json');
+const bsc_testnet = require('./tokens/bsc-testnet.json');
+
+module.exports = function buildList() {
+  const parsed = version.split('.');
+  return {
+    'name': 'Art Economics Default List',
+    'timestamp': (new Date().toISOString()),
+    'version': {
+      'major': +parsed[ 0 ],
+      'minor': +parsed[ 1 ],
+      'patch': +parsed[ 2 ]
+    },
+    'tags': {},
+    'logoURI': 'https://raw.githubusercontent.com/Artnomics-org/ane-default-token-list/main/src/icon-512.png',
+    'keywords': [
+      'uniswap',
+      'default'
+    ],
+    tokens: [
+      ...bsc_mainnet,
+      ...bsc_testnet,
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      })
+  };
+};
